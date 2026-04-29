59f52a5dd454edda177a441391cffb1d8a47a647
staslolpol <staspriho9@gmail.com>
2026-04-29 17:03:11 +0300

    Add project files.

---
+++ Task01/main.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() {
+//	int* ptr = NULL;
+//
+//	ptr = new int;
+//
+//	*ptr = 10;
+//
+//	cout << "address = " << ptr << " - value = " << *ptr;
+//
+//	delete ptr;
+//
+//	return 0;
+//}
