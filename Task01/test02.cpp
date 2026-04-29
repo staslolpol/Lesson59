f7d2b87dc07ce20e8d209511d985fd2fe47a3f47
staslolpol <staspriho9@gmail.com>
2026-04-29 17:27:04 +0300

    update 01

---
+++ Task01/test02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int size = 10;
+
+	int* array = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		*(array + i) = 0; 
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(array + i) << "\t";
+	}
+
+	delete[] array;
+	
+	return 0;
+}
